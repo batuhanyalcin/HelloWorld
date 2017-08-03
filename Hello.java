010fd313aa410d934205ade81daf3ea27c7286eb
@batuhan.yalcin <by.yalcin.batuhan@gmail.com>
2017-08-03 15:19:41 +0300

    small error

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ public class Hello{
 
 	public static void main(String[] args){
 	
-		for(int i=1;i<=20;i++){
-			System.println(a + i);
+		for(int i=1;;i++){
+			System.out.println("Hello World ... " + i);
 		}
 	
 	}
